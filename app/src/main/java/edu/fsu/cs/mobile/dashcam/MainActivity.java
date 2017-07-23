7ee0b2661624ee594cf6eadad8ed9cc3a921aeb7
sap15e <anjalibrown12@gmail.com>
2017-07-23 16:01:22 -0400

    Pushing VideoFragment.java -anjali

--- app/src/main/java/edu/fsu/cs/mobile/dashcam/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/mobile/dashcam/MainActivity.java
@@ -1,13 +1,36 @@
 package edu.fsu.cs.mobile.dashcam;
 
+import android.content.ContentValues;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements VideoFragment.VideoFragmentListener
+{
 
+
+    //THIS IS A COMMENT
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onMain();
     }
+
+    @Override
+    public void onRecord(ContentValues values)
+    {
+
+
+
+    }
+
+    public void onMain()
+    {
+        MainFragment fragment;
+    }
+
+
+
 }
+
---
+++ app/src/main/java/edu/fsu/cs/mobile/dashcam/VideoFragment.java
@@ -0,0 +1,82 @@
+package edu.fsu.cs.mobile.dashcam;
+
+/**
+ * Created by sap15e on 7/23/2017.
+ */
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
+public class VideoFragment extends Fragment{
+
+    private VideoFragmentListener mListener;
+    private View mRootView;
+
+    public VideoFragment()
+    {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mRootView = inflater.inflate(R.layout.video_layout, container, false);
+        // TODO: Setup UI
+        //ListView list;
+        //list = mRootView.findViewById(R.id.list_url);
+        TextView text;
+
+
+
+
+
+        return mRootView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof VideoFragmentListener) {
+            mListener = (VideoFragmentListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement ViewFragmentListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface VideoFragmentListener
+    {
+        // void onRecord(ContentValues values);
+    }
+
+}
+
