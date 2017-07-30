3b946678ea60336c0b5b3264c7b6bf550c0e4a5c
garrettschmitt <garr.schm@gmail.com>
2017-07-30 16:44:32 -0400

    -video player now plays recorded video, for now displays error toast if user has never recorded a video

--- app/src/main/java/edu/fsu/cs/mobile/dashcam/VideoPlayer.java
+++ app/src/main/java/edu/fsu/cs/mobile/dashcam/VideoPlayer.java
@@ -9,7 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
+
+import java.io.File;
+import java.net.URI;
 
 
 /**
@@ -31,12 +35,20 @@ public class VideoPlayer extends Fragment
         VideoView vidView = (VideoView) rootView.findViewById(R.id.myVideo);
 
         //Uri vidUri = Uri.parse(uri);
-        String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
-        vidView.setVideoURI(Uri.parse(vidAddress));
-        vidView.start();
-        MediaController vidControl = new MediaController(this.getContext());
-        vidControl.setAnchorView(vidView);
-        vidView.setMediaController(vidControl);
+        //String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+        String vidPath = "file:///storage/emulated/0/DCIM/MapCam/VID_MapCam_Recording.mp4";
+        File videoFile = new File(URI.create(vidPath).getPath());
+
+        if (videoFile.exists()) {
+
+            vidView.setVideoURI(Uri.parse(vidPath));
+            vidView.start();
+            MediaController vidControl = new MediaController(this.getContext());
+            vidControl.setAnchorView(vidView);
+            vidView.setMediaController(vidControl);
+        } else {
+            Toast.makeText(getContext(), "Sorry no recording to show", Toast.LENGTH_LONG).show();
+        }
 
         return rootView;
     }
